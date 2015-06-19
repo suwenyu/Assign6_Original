afef9117cdb7f0633c0a1a85aa0be41ef3f20095
suwenyu <cy_i90118@yahoo.com.tw>
2015-06-19 20:20:24 +0800

    change the key

--- js/key.js
+++ js/key.js
@@ -1,2 +1,2 @@
 //Parse.initialize
-Parse.initialize("PkODXEomm2A789yYWQJfWdX2WJFx6Xo34qms8CTb", "qH4gNdk6vN4dMkFe8TToWoVPnWkBe6bB2tcCgwuR");
+Parse.initialize("4rMkOtjdLnCtAbW5Fw7LrtAucpAXGg01sCENcnGn", "TM7k3IPJyhK8LzHDurjzQaEkgq68X2r1I5QYmleu");
